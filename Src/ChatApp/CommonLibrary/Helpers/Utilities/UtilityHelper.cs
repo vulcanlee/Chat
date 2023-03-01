9da305a3556d584468e790e55212dd3bb0d1ee37
Vulcan <vulcan.lee@gmail.com>
2023-03-01 11:53:32 +0800

    移植 Xamarin.Forms 專案內常用的 API 到 .NET Maui 內

---
+++ Src/ChatApp/CommonLibrary/Helpers/Utilities/UtilityHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonLibrary.Helpers.Utilities
+{
+    public class NetworkHelper
+    {
+        /// <summary>
+        /// Gets if there is an active internet connection
+        /// </summary>
+        /// <returns></returns>
+        public static bool IsConnected()
+        {
+            var current = Connectivity.Current.NetworkAccess;
+
+            if (current == NetworkAccess.Internet)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
